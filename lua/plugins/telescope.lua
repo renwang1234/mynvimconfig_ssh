1c9258fdd248601eda2f8d238862e2df288a44df
Ren <ren.fu.wong0802@gmail.com>
2026-05-02 00:45:30 +0800

    update telescope.lua

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,1 +1,10 @@
-return {}
+return {
+  "nvim-telescope/telescope.nvim",
+  version = "*",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "mingw32-make" },
+  },
+  opts = {},
+}
